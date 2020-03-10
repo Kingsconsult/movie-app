9fc79677d7f94d28db44b24d2071c4b886542b5e
Oluwasayo <kingsconsult001@gmail.com>
2020-03-11 00:44:33 +0100

    created the develop branch and created the models

--- backend/movieapi/models.py
+++ backend/movieapi/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+     STATUSES = (
+          ('UNKNOWN', 'unknown'),
+          ('FREE', 'free'),
+          ('PAID', 'paid')
+     )
+     title = models.CharField(max_length=36,  unique=True, null=True)
+     description = models.TextField(max_length=256, blank=True)
+     subscription_type = models.CharField(max_length=9, choices=STATUSES)
+     price = models.DecimalField(max_digits=1000, decimal_places=2, default=0)
+     published_date = models.DateField(blank=True, null=True, default=None)
+     cover = models.ImageField(upload_to='covers/')
